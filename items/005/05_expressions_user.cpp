cb82afe2885d99d094bad5c8cbceadb873ed01ae
manstetten <manstetten@iue.tuwien.ac.at>
2021-10-22 12:09:42 +0200

    lecture item 005 update

--- items/005/05_expressions_user.cpp
+++ items/005/05_expressions_user.cpp
@@ -1,8 +1,36 @@
+struct Widget {
+  int i;
+  // operator overloads as member functions
+  // invocation: Widget c = a + b;
+  // invocation: Widget c = a.member(b);
+  auto member(const Widget &other) const {
+      // "a" "*this" is the object
+      // "b" "other" is just an ordinary argument  
+      // if i just type "i" in this context?
+      // -> lookup first happens in "this"
+      return Widget{this->i + other.i}; 
+  }
+  auto operator-(const Widget &other) const { return Widget{i - other.i}; }
+  auto operator*(const Widget &other) const { return Widget{i * other.i}; }
+  Widget(int i) : i(i){}; // converting constructor for 'int' to 'Widget'
+  Widget()
+      : i{} {}; // explicit default constructor (implicit version is deleted due
+                // to existence of user-defined constructor)
+};
+
+// operator overload as free function
+// this template would be considerend for any type T, but will fail to be
+// instantiated for incompatible types. how to restrict T to a strict subset of
+// types which are known to be compatible, e.g. integral types?
+template <typename T> auto operator+(const T &maywork, const Widget &widget) {
+  return T{maywork + widget.i}; 
+};
+
 int main() {
   {
-    struct Widget {
-      int i;
-    };
+    // struct Widget {
+    //   int i;
+    // };
     using Type = Widget;
     Type a{};
     Type b{};
